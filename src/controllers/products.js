f09ab7c9a6e1e9b53406cc624c86bd3c3e006b28
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-27 17:25:32 -0300

    finished store api

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,28 +1,62 @@
 import Product from "../models/product.js";
 
 export async function getAllProducts(req, res) {
-   const { featured, name, company } = req.query;
+   const { featured, name, company, sort, field, numericFilters } = req.query;
    const queryObj = {};
 
+   if (name) {
+      queryObj.name = { $regex: name, $options: "i" };
+   }
    if (featured) {
       queryObj.featured = featured == "true" ? true : false;
-   }
-   if (name) {
-      queryObj.name = name;
    }
    if (company) {
       queryObj.company = company;
    }
+   if (numericFilters) {
+      const operatorMap = {
+         ">": "$gt",
+         ">=": "$gte",
+         "=": "$eq",
+         "<": "$lt",
+         "<=": "$lte",
+      };
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+      let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+      const options = ["price", "rating"];
+      filters = filters.split(",").forEach((item) => {
+         const [field, operator, value] = item.split("-");
+         if (options.includes(field)) {
+            queryObj[field] = { [operator]: Number(value) };
+         }
+      });
+   }
 
-   console.log(queryObj);
+   let result = Product.find(queryObj);
 
-   const products = await Product.find(queryObj);
+   if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+   } else {
+      result = result.sort("createdAt");
+   }
+
+   if (field) {
+      const fieldList = field.split(",").join(" ");
+      result = result.select(fieldList);
+      console.log(fieldList);
+   }
+   const page = Number(req.query.page) || 1;
+   const limit = Number(req.query.limit) || 10;
+
+   const skip = (page - 1) * limit;
+   result = result.skip(skip).limit(limit);
+
+   const products = await result;
    res.status(200).json({ nbHits: products.length, products });
 }
 
 export async function getAllProductsStatic(req, res) {
-    const products = await Product.find({
-        name:'accent chair'
-    })
-    res.status(200).json({nbHits: products.length, products})
+   const products = await Product.find({}).sort("name price");
+   res.status(200).json({ nbHits: products.length, products });
 }
